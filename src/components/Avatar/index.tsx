f1e3d72e92c193cef263e43980678a9d86a4fb87
Emerson Freitas <emersonfreitasn@gmail.com>
2022-07-14 18:33:38 -0500

    avatar component

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,9 @@
+import { AvatarContainer, Image } from "./styles";
+
+export function Avatar() {
+  return (
+    <AvatarContainer>
+      <Image src="https://avatars.githubusercontent.com/u/29461618?v=4" />
+    </AvatarContainer>
+  );
+}
---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const AvatarContainer = styled.div``;
+
+export const Image = styled.img`
+  display: block;
+
+  height: 98px;
+
+  border: 1px solid #7434c3;
+  border-radius: 50%;
+
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
+    0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`;
