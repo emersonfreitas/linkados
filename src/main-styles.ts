15966c746d8fae3791aa606884e68521c8fdf421
Emerson Freitas <emersonfreitasn@gmail.com>
2022-07-14 17:42:07 -0500

    changing global css

---
+++ src/main-styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  :root {
+    font-family: "Poppins", sans-serif;
+  }
+
+  body {
+    background-color: #333639;
+  }
+`;
