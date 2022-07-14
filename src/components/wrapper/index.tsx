124124f008514131ffb3297ca8a0759e7c0f3185
Emerson Freitas <emersonfreitasn@gmail.com>
2022-07-14 17:41:44 -0500

    wrapper component

---
+++ src/components/wrapper/index.tsx
@@ -0,0 +1,9 @@
+import { WrapperArea } from "./styles";
+
+export function Wrapper() {
+  return (
+    <WrapperArea>
+      <h1>Wrapper</h1>
+    </WrapperArea>
+  );
+}
---
+++ src/components/wrapper/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const WrapperArea = styled.div`
+  margin: 0 auto;
+  width: 100vw;
+  max-width: 1280px;
+  min-height: 100vh;
+  color: #fefefe;
+
+  display: flex;
+  justify-content: center;
+`;
